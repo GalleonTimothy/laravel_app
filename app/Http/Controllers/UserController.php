7b76a3a9633dfdd190991a4f59b2babfc01f6439
N3002 User <N3002user@N3002.uc-bcf.edu.ph>
2024-02-14 09:31:38 +0800

    sample push

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,12 @@ class UserController extends Controller
         return 'Hello from UserController';
     }
 
-    public function goto(){
-        return view('Users');
+    // public function goto(){
+    //     return view('Users');
+    // }
+
+    public function show($id){
+        return $id;
+
     }
 }
